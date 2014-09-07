d27f28829cc3cb16c68144776bd25501c6aad38c
Stefan Lemme <stefan.lemme@dfki.de>
2014-09-07 22:16:07 +0200

    polish pois
    * new poi asset
    * animate poi markers
    * building examples for SB and M

--- config.js
+++ config.js
@@ -6,7 +6,9 @@ var global = {
 		"img_tiles": "http://130.206.80.175/api/tiles/filab/${z}/${x}/${y}.png",
 		"ground_tiles": {
 			"osmde": "http://130.206.80.175/api/3d-map-tiles/filab",
-			"gis": "http://130.206.80.175/api/3d-map-tiles/oulu"
+			"gis": "http://130.206.80.175/api/3d-map-tiles/oulu",
+			"sb": "http://localhost/api/3d-map-tiles/sb",
+			"tum": "http://localhost/api/3d-map-tiles/tum"
 		}
 	}
 };
--- js/3d-map-tiles.js
+++ js/3d-map-tiles.js
@@ -257,6 +257,8 @@ Xflow.registerOperator("xflow.extrudePolygon", {
             position[6*i+5] = contour[3*i+2];
         }
 
+		
+
 		// generate indices for the walls
         for (var i = 0; i < points; i++)
 		{
@@ -431,6 +433,49 @@ Xflow.registerOperator("xflow.generateFaceNormal", {
 				normal[C+j] = N[j];
 			}
         }
+		return true;
+    }
+});
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+Xflow.registerOperator("xflow.ensureCCWContour", {
+    outputs: [
+		{type: 'float2', name: 'out_contour'}
+	],
+    params:  [
+        {type: 'float2', source: 'in_contour'}
+    ],
+    evaluate: function(out_contour, in_contour, info)
+	{
+
+		var n = info.iterateCount; // this.points;
+		var tn = 2*n;
+		var A = 0.0;
+
+		for (var p=n-1, q=0; q<n; p=q++)
+			A += in_contour[2*p]*in_contour[2*q+1] - in_contour[2*q]*in_contour[2*p+1];
+	
+		if (0.5*A < 0.0) {
+			// reverse order of vertices
+			for (var i = 0; i < tn; i+=2) {
+				out_contour[i  ] = in_contour[tn-i-2];
+				out_contour[i+1] = in_contour[tn-i-1];
+			}
+			
+		} else {
+			// leave order as it is
+			for (var i = 0; i < tn; i+=2) {
+				out_contour[i  ] = in_contour[i  ];
+				out_contour[i+1] = in_contour[i+1];
+			}
+			
+		}
+
 		return true;
     }
 });
