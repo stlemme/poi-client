485097360a171606bbab1db5a785802a0328f7d3
Stefan Lemme <stefan.lemme@dfki.de>
2014-11-25 17:25:47 +0100

    fix NaN issue of animations

--- js/xml3d-animation.js
+++ js/xml3d-animation.js
@@ -6,7 +6,6 @@ var XML3D = XML3D || {};
 XML3D.animators = Array();
 
 XML3D.Animator = function() {
-	this.lastTime = new Date().getTime();
 	this.anims = Array();
 	
 	XML3D.animators.push(this);
@@ -23,10 +22,12 @@ XML3D.Animator.prototype.registerAnimation = function (anim) {
 };
 
 
+var lastTime = new Date().getTime();
+
 (XML3D.animloop = function () {
 	var t = new Date().getTime();
-	var delta = (t - this.lastTime) / 1000;
-	this.lastTime = t;
+	var delta = (t - lastTime) / 1000;
+	lastTime = t;
 	window.requestAnimationFrame(XML3D.animloop);
 	// console.log(delta);
 	XML3D.animators.forEach (function (anim) {
