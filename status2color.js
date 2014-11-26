3270fb4eaa81f386893af3a8c1d8153a6cb62211
Stefan Lemme <stefan.lemme@dfki.de>
2014-11-26 14:45:39 +0100

    correct color of unknown status

--- status2color.js
+++ status2color.js
@@ -2,7 +2,7 @@
 // TODO: adapt colors to be consistent with 2d map
 
 var status2color = [
-	[0.700, 0.700, 0.700],  // unknown
+	[0.561, 0.561, 0.561],  // #8f8f8f - unknown
 	[0.149, 0.647, 0.224],  // #26a539 - normal
 	[0.929, 0.427, 0.114],  // #ed6d1d - busy
 	[0.898, 0.145, 0.129],  // #e52521 - crowded
